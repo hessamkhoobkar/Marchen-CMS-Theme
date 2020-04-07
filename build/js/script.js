df4467fe7089acdebb489df86ecd44db8059e1e4
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2020-04-07 22:28:20 +0430

    Off Canvas panels

---
+++ build/js/script.js
@@ -0,0 +1,31 @@
+// notification_panel
+$('#notification_toggle').click(function () {
+    $('.notification_panel').addClass('show')
+});
+$('#notification_close').click(function () {
+    $('.notification_panel').removeClass('show')
+});
+
+// schedule_panel
+$('#schedule_toggle').click(function () {
+    $('.schedule_panel').addClass('show')
+});
+$('#schedule_close').click(function () {
+    $('.schedule_panel').removeClass('show')
+});
+
+// setting_panel
+$('#setting_toggle').click(function () {
+    $('.setting_panel').addClass('show')
+});
+$('#setting_close').click(function () {
+    $('.setting_panel').removeClass('show')
+});
+
+// search_panel 
+$('#search_toggle').click(function () {
+    $('.search_panel').addClass('show')
+});
+$('#search_close').click(function () {
+    $('.search_panel').removeClass('show')
+});
